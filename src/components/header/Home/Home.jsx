930ce7a9a6480031d3fddb954c1e4aba74bf8480
Varun Gopinath <varun.g.nath.official@gmail.com>
2024-12-27 20:05:16 -0500

    midcommit

--- src/components/header/Home/Home.jsx
+++ src/components/header/Home/Home.jsx
@@ -1,2 +1,15 @@
-// src\components\header\Home\Home.jsx
+import React from 'react'
 
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+        <div className="home__container container grid">
+            <div className="home__content grid">
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/components/header/Home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon" target="_blank"> 
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="" className="home__social-icon" target="_blank"> </a>
+
+        <a href="" className="home__social-icon" target="_blank"> </a>
+    </div>
+  )
+}
+
+export default Social
