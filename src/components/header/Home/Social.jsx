91d987254a4b2813cc6fc4136c6401654a1d128c
Varun Gopinath <varun.g.nath.official@gmail.com>
2024-12-28 12:52:30 -0500

    Data Filed

--- src/components/header/Home/Social.jsx
+++ src/components/header/Home/Social.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_blank"> 
+        <a href="https://www.instagram.com/varun.g.nath/" className="home__social-icon" target="_blank"> 
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="" className="home__social-icon" target="_blank"> </a>
+        <a href="www.linkedin.com/in/varungnath" className="home__social-icon" target="_blank"> 
+            <i class="uil uil-dribbble"></i>
+        </a>
 
-        <a href="" className="home__social-icon" target="_blank"> </a>
+        <a href="https://github.com/varungnathan" className="home__social-icon" target="_blank"> 
+            <i class="uil uil-github"></i>
+        </a>
     </div>
   )
 }
 
-export default Social
+export default Social 
