b1093de368b5db85056eb71868ec2ce4e9de64e6
Varun Gopinath <varun.g.nath.official@gmail.com>
2025-01-02 13:08:11 -0500

    Add Qualification component with education and experience sections, and include associated styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import Home from './components/header/Home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
+// import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
 
 const App = () => {
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -9,14 +9,44 @@ const Qualification = () => {
 
     <div className="qualification__container container">
         <div className="qualification__tabs">
-            <div className="qualification__button button--flex">
-               <i className="uil uil-graduation-cap qualification__icon"></i>Education
+            <div className="qualification__button qualification__active button--flex">
+               <i className="uil uil-graduation-cap qualification__icon"></i> Education
             </div>
 
             <div className="qualification__button button--flex">
-               <i className="uil uil- qualification__icon"></i> 
+               <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
             </div> 
         </div>
+
+        <div className="qualification__sections">
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <h3 className="qualification__title">Web Development </h3>
+              <span className="qualification__subtitle">Conestoga College</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2023 - 2024
+                </div>
+            </div>
+
+            <div>
+              <span className="qualification__rounder"></span>
+              <span className="qualification__line"></span>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+              <div>
+              <span className="qualification__rounder"></span>
+              <span className="qualification__line"></span>
+            </div>
+              <h3 className="qualification__title">Bachelors Degree in Computer Science & Engineering</h3>
+              <span className="qualification__subtitle">Mangalam College of Engineering</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2017 - 2021
+                </div>
+            </div>
+          </div>
+        </div>   
     </div>
     </section>
   )
