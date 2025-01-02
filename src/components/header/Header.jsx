014b376c4f5f4264123ac831af0f862ccbb0a3c8
Varun Gopinath <varun.g.nath.official@gmail.com>
2025-01-02 17:26:56 -0500

    Update navigation links in Header component for clarity

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,14 +31,14 @@ const Header = () => {
                     </li>
 
                     <li className="nav__item">
-                        <a href="#services" className="nav__link">
-                            <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                        <a href="#portfolio" className="nav__link">
+                            <i className="uil uil-scenery nav__icon"></i> Education
                         </a>
                     </li>
 
                     <li className="nav__item">
                         <a href="#portfolio" className="nav__link">
-                            <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            <i className="uil uil-scenery nav__icon"></i> Projects
                         </a>
                     </li>
 
