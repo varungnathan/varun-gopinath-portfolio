1c15f9cfe4648411f09ca9cb1b216253bc19c610
Varun Gopinath <varun.g.nath.official@gmail.com>
2025-03-14 21:50:17 -0400

    Refactor App component: clean up code, enhance smooth scrolling functionality, and remove commented-out sections

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 // src\App.js
 
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import routing components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/header/Home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Education from './components/education/Education';
-import Projects from './components/projects/Projects'; // Import Projects component
-import Resume from './components/resume/Resume'; // Import Resume component
+import Projects from './components/projects/Projects';
+import Resume from './components/resume/Resume';
 import Contact from './components/contact/Contact';
-// import Paper from './components/paper/Paper';
 
 const App = () => {
   useEffect(() => {
-    // Add smooth scrolling only on the Home page
     if (window.location.pathname === '/') {
       document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
           e.preventDefault();
-
-          const targetId = this.getAttribute('href'); // Get the target ID (e.g., "#about")
-          const targetElement = document.querySelector(targetId); // Find the target element
-
+          const targetId = this.getAttribute('href');
+          const targetElement = document.querySelector(targetId);
           if (targetElement) {
             targetElement.scrollIntoView({
               behavior: 'smooth',
@@ -38,7 +34,6 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        {/* Home Page Route */}
         <Route
           path="/"
           element={
@@ -52,8 +47,6 @@ const App = () => {
             </main>
           }
         />
-
-        {/* Resume Page Route */}
         <Route path="/resume" element={<Resume />} />
       </Routes>
     </Router>
